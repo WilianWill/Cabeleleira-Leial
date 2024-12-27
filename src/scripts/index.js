0cded7271a2eb972933b641b6f3c4363e7e9cec7
Wilian <williangoncalvespp@hotmail.com>
2024-12-27 09:34:31 -0300

    add faq function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -92,3 +92,27 @@ document.addEventListener("DOMContentLoaded", () => {
   animarMenu();
   window.addEventListener("scroll", animarMenu);
 });
+
+function activeFaq() {
+  const faq_title = document.querySelectorAll(".faq_title");
+
+  faq_title.forEach((faq) => {
+    faq.addEventListener("click", () => {
+      // Verifica se o item clicado já está ativo
+      const isActive = faq.classList.contains("active");
+
+      // Remove a classe 'active' de todos os itens
+      faq_title.forEach((title) => {
+        title.classList.remove("active");
+        title.nextElementSibling?.classList.remove("active");
+      });
+
+      // Se o item clicado não estava ativo, ativa-o
+      if (!isActive) {
+        faq.classList.add("active");
+        faq.nextElementSibling?.classList.add("active");
+      }
+    });
+  });
+}
+activeFaq();
